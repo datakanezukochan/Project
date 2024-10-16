ed9384ae67676ef354905b1bb887d030222b89da
Tdee <101107770+datakanezukochan@users.noreply.github.com>
2024-10-16 16:46:29 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,41 +1,48 @@
 
 # Library
+import random
+import pygame
 import time
-from word_bank import hangman_word
-from time import sleep
-from sys import stdout
+from initial_board import initial_board
+from word_bank import easy_words, hard_words, idioms_list
+import speech_recognition as sr
 
-def display_board():
-    for i in range(0, 1000):
-        stdout.write("\b")
-        sleep(.9)
-        stdout.write("\b\b\b>>>" + str(i))
-        stdout.flush()
 
-def correct_char(word, char):
-    if char in word:
-        return True
-    
-    return False
+r = sr.Recognizer()
+update_display = 0
+################################################################################################
 
-def list_characters():
-    pass
 
-def play_game():
-    word = hangman_word(word)
-    word_completion = "_ " * len(word)
+def player_version():
+    choice = input("What version do you wanna play? Easy or hard.\n We also have the version 'idiom' or 'reversed version'")
+
+
+def guessing(word, guess, blank_word):
+    global update_display
+    correct_guess = False
+    for i in word:
+        if len(guess) != 1:
+            if guess == word:
+                blank_word = word
+                return True
+        else:
+            if guess.upper() == word[i]: 
+                blank_word[i] = word[i]
+                correct_guess = True
+
+    if correct_guess == False:
+        print(f"There is no {guess.upper()}!! Please try again :)")
+        update_display += 1
+        tries -= 1
+
+
+def play_game(word):
+    word_completion = "_" * len(word)
     guess = False
     tries = 6 
     print("I bet you can't win this game! Let's play Hangman")
-    print(display_board(tries))
+    # print(display_board(tries))
     print(word_completion)
     while not guess and tries > 0:
-        guess = input("Please guess a letter or word: ").upper()
-        if len(guess) == 1 and correct_char(word, guess):
-            pass
-        elif guess == word:
-            pass
-        else: 
-            print("Not a correct one, try again!!")
-            tries -= 1
+        pass
 
